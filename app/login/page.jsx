64b916b86b37eaccb20207dd418c285da0a942ed
andersongama-dev <andersongamasilva5@gmail.com>
2026-02-02 10:24:29 -0300

    CSS login the end

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -24,7 +24,7 @@ export default function Login() {
 
       <section className="banner-initial">
         <img
-          src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2Foriginals%2Fa5%2Fa5%2F59%2Fa5a55933ade2e2d5f90a95c0aa2c443f.jpg&f=1&nofb=1&ipt=aa0ba2753a1a1cace47fc6c0a3516bb6864304e40cfb68298039078d46386c9f"
+          src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimages.unsplash.com%2Fphoto-1621645582931-d1d3e6564943%3Fixlib%3Drb-4.0.3%26ixid%3DMnwxMjA3fDB8MHxzZWFyY2h8MTB8fGJhcmJlcnNob3B8ZW58MHx8MHx8%26w%3D1000%26q%3D80&f=1&nofb=1&ipt=c521c1f0b8c36b7518b0285c3ae9a3d83700e3cd7e2372820a511dec3141d35b"
           alt="Image barbe shop"
         />
       </section>
